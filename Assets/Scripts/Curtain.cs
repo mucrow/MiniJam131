5c0d496d13cdeac1ec9dc53db3f754eade1d0bf7
Dolly Pham <doh.draws@gmail.com>
2023-04-30 14:08:43 -0700

    modify level2, curtain.cs

--- Assets/Scripts/Curtain.cs
+++ Assets/Scripts/Curtain.cs
@@ -2,42 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Curtain: MonoBehaviour {
-  float _curtainOpenTimer = 2f;
+public class Curtain : MonoBehaviour
+{
+    float _curtainOpenTimer = 2f;
 
-  [SerializeField] Sprite _openCurtainSprite;
-  [SerializeField] Sprite _closedCurtainSprite;
-  [SerializeField] SpriteRenderer _spriteRenderer;
-  [SerializeField] LightSource _lightSource;
+    [SerializeField] Sprite _openCurtainSprite;
+    [SerializeField] Sprite _closedCurtainSprite;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] LightSource _lightSource;
+    [SerializeField] HorizontalLightSource _horizontalLightSource;
 
-  [SerializeField] bool _isOpen = false;
+    [SerializeField] bool _isOpen = false;
 
-  void Start() {
-    OnIsOpenChanged();
-  }
-
-  // Update is called once per frame
-  void Update() {
-    _curtainOpenTimer -= Time.deltaTime;
-    if (_curtainOpenTimer <= 0f) {
-      _curtainOpenTimer = 2f;
-      ToggleOpen();
+    void Start()
+    {
+        OnIsOpenChanged();
     }
-  }
 
-  void ToggleOpen() {
-    _isOpen = !_isOpen;
-    OnIsOpenChanged();
-  }
+    // Update is called once per frame
+    void Update()
+    {
+        _curtainOpenTimer -= Time.deltaTime;
+        if (_curtainOpenTimer <= 0f)
+        {
+            _curtainOpenTimer = 2f;
+            ToggleOpen();
+        }
+    }
 
-  void OnIsOpenChanged() {
-    if (_isOpen) {
-      _spriteRenderer.sprite = _openCurtainSprite;
-      _lightSource.Show();
+    void ToggleOpen()
+    {
+        _isOpen = !_isOpen;
+        OnIsOpenChanged();
     }
-    else {
-      _spriteRenderer.sprite = _closedCurtainSprite;
-      _lightSource.Hide();
+
+    void OnIsOpenChanged()
+    {
+        if (_isOpen)
+        {
+            _spriteRenderer.sprite = _openCurtainSprite;
+            if (_lightSource is not null)
+            {
+                _lightSource.Show();
+
+            }
+            if (_horizontalLightSource is not null)
+            {
+                _horizontalLightSource.Show();
+            }
+        }
+        else
+        {
+            _spriteRenderer.sprite = _closedCurtainSprite;
+            if (_lightSource is not null)
+            {
+                _lightSource.Hide();
+
+            }
+            if (_horizontalLightSource is not null)
+            {
+                _horizontalLightSource.Hide();
+            }
+        }
     }
-  }
 }
