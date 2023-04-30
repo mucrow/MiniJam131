efbc760df9fef5417c5037f910bdaa49b3655512
mucrow <120820315+mucrow@users.noreply.github.com>
2023-04-30 19:01:01 -0400

    Talk to level manager through singleton variable

--- Assets/Scripts/Glow.cs
+++ Assets/Scripts/Glow.cs
@@ -6,15 +6,13 @@ using UnityEngine;
 public class Glow: MonoBehaviour {
   [SerializeField] AudioSource _audioSource;
   [SerializeField] AudioClip _deathSound;
-  [SerializeField] LevelManager _levelManager;
 
   void OnTriggerEnter2D(Collider2D collision) {
     if (collision.CompareTag("Player")) {
       if (collision.GetComponent<Dracula>()._isHuman) {
         Destroy(collision.gameObject);
         _audioSource.PlayOneShot(_deathSound);
-        _levelManager.OnPlayerDied();
-
+        LevelManager.Instance.OnPlayerDied();
       }
     }
   }
