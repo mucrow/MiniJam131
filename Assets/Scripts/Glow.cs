0fd49427a15f87af1caa14c5fd35d7d6a557896e
Dolly Pham <doh.draws@gmail.com>
2023-04-30 17:01:40 -0700

    horizontal glow retract diferently, also add tag to glow prefaB

--- Assets/Scripts/Glow.cs
+++ Assets/Scripts/Glow.cs
@@ -3,39 +3,58 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Glow: MonoBehaviour {
-  [SerializeField] AudioSource _audioSource;
-  [SerializeField] AudioClip _deathSound;
+public class Glow : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _deathSound;
 
+    private int deaths;
 
-  Vector3 scaleChange = new Vector3(0.5f, 0f, 0f);
-
-  private int deaths;
-
-  void Start() {
-    deaths = LevelManager.Instance.GetDeathCount();
-    ShrinkLight(deaths);
-  }
-
-  void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.CompareTag("Player")) {
-      if (collision.GetComponent<Dracula>()._isHuman) {
-        Destroy(collision.gameObject);
-        _audioSource.PlayOneShot(_deathSound);
-        LevelManager.Instance.OnPlayerDied();
-      }
+    void Start()
+    {
+        deaths = LevelManager.Instance.GetDeathCount();
+        ShrinkLight(deaths);
     }
-  }
 
-  void ShrinkLight(int deathCount)
-  {
-    if (deathCount > 0 && deathCount < 3) {
-        transform.localScale = Vector3.Scale(transform.localScale, new Vector3(0.8f, 1, 1));
-    } else if ( deathCount >= 3 && deathCount < 5) {
-        transform.localScale = Vector3.Scale(transform.localScale, new Vector3(0.7f, 1, 1));
-    } else if ( deathCount >= 5){
-        transform.localScale = Vector3.Scale(transform.localScale, new Vector3(0.5f, 1, 1));
-    } 
-  }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.GetComponent<Dracula>()._isHuman)
+            {
+                Destroy(collision.gameObject);
+                _audioSource.PlayOneShot(_deathSound);
+                LevelManager.Instance.OnPlayerDied();
+            }
+        }
+    }
+
+    void ShrinkLight(int deathCount)
+    {
+        if (deathCount > 0 && deathCount < 3)
+        {
+            ScaleCorrectDirection(tag, 0.8f);
+        }
+        else if (deathCount >= 3 && deathCount < 5)
+        {
+            ScaleCorrectDirection(tag, 0.7f);
+        }
+        else if (deathCount >= 5)
+        {
+            ScaleCorrectDirection(tag, 0.5f);
+        }
+    }
+
+    void ScaleCorrectDirection(string tag, float amount){
+        if (tag == "Horizontal")
+            {
+                transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1, amount, 1));
+
+            }
+            else
+            {
+                transform.localScale = Vector3.Scale(transform.localScale, new Vector3(amount, 1, 1));
+            }
+    }
 
 }
